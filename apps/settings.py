fc7ff9d642a8e61d8cf9471813e624ccb4f1d004
Dave Fogelson <github@landfillinc.com>
2012-03-22 14:29:41 -0400

    Fix the static root for production

--- apps/settings.py
+++ apps/settings.py
@@ -52,7 +52,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dfogelson/projects/real_estate_manager/static'
+STATIC_ROOT = '/var/www/real_estate/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
